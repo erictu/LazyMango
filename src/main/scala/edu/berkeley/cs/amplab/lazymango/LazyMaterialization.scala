5a2c2e70ad18d81828cbbcd9ec3a2781820ae968
Eric Tu <erictctu@gmail.com>
2015-10-08 21:04:07 -0700

    compiles, added in tree logic

--- src/main/scala/edu/berkeley/cs/amplab/lazymango/LazyMaterialization.scala
+++ src/main/scala/edu/berkeley/cs/amplab/lazymango/LazyMaterialization.scala
@@ -29,7 +29,6 @@ import org.apache.spark.SparkContext
 import org.apache.spark.TaskContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
-// import org.bdgenomics.adam.models._
 import org.bdgenomics.adam.projections.{ Projection, VariantField, AlignmentRecordField, GenotypeField, NucleotideContigFragmentField, FeatureField }
 import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.formats.avro.{ AlignmentRecord, Feature, Genotype, GenotypeAllele, NucleotideContigFragment }
@@ -38,30 +37,54 @@ import scala.collection.mutable.ListBuffer
 
 
 //TODO: Have it do things other than AlignmentRecord, currently just trying it out
-class LazyMaterialization(input: RDD[(Interval[Long], (String, AlignmentRecord))], filePath: String, sc: SparkContext) {
+class LazyMaterialization(filePath: String, sc: SparkContext) {
 	// chromosome, whether the interval for that chromosome exists
-	val bookkeep: IntervalTree[Long, Boolean] = new IntervalTree[Long, Boolean]()
+	val bookkeep: IntervalTree[String, Boolean] = new IntervalTree[String, Boolean]()
 
 	// TODO: if file/files provided does not have have all the chromosomes, how do we fetch files?
 	// file path of file we're lazily materializing. Currently it's only one file
 	val fp: String = filePath
 	// TODO: This correct types for alignment record?
-	val intRDD: IntervalRDD[Interval[Long], String, AlignmentRecord] = IntervalRDD(input)
+	var intRDD: IntervalRDD[Interval[Long], String, AlignmentRecord] = null
 
 
 
-	/* Gets the data for an interval for the file loaded by checking in the bookkeeping tree.
+
+	/* If the RDD has not been initialized, initialize it to the first get request
+	* Gets the data for an interval for the file loaded by checking in the bookkeeping tree.
 	* If it exists, call get on the IntervalRDD
 	* Otherwise call put on the sections of data that don't exist
 	*/
   	def multiget(chr: String, intl: Interval[Long], ks: Option[List[String]]): Option[Map[Interval[Long], List[(String, AlignmentRecord)]]] = {
-  		var data = intRDD.multiget(chr, intl, ks)
-  		data match {
-  			case Some(_) => {
-  				return data
-  			}
-  			case None => put(chr, intl, ks)
+  		// Initialize the RDD to the first get request
+  		if (intRDD == null) {
+	        val pred: FilterPredicate = ((LongColumn("end") >= intl.start) && (LongColumn("start") <= intl.end))
+	        val proj = Projection(AlignmentRecordField.contig, AlignmentRecordField.readName, AlignmentRecordField.start, AlignmentRecordField.end, AlignmentRecordField.sequence, AlignmentRecordField.cigar, AlignmentRecordField.readNegativeStrand, AlignmentRecordField.readPaired)
+	        val loading = sc.loadParquetAlignments(fp, predicate = Some(pred), projection = Some(proj))
+	  		val ready: RDD[(Interval[Long], (String, AlignmentRecord))] = loading.map(rec => (new Interval(rec.start, rec.end), ("person1", rec)))
+	  		intRDD = IntervalRDD(ready)
+
+	  		// Add our initial entry into our tree, then call get again, now with the data loaded
+	  		bookkeep.insert(intl, (chr, true))
+	  		multiget(chr, intl, ks)
   		}
+  		else {
+  			// ks is an Option, so get it
+  			val searchResults: List[(String, Boolean)] = bookkeep.search(intl, ks.get)
+  			// Put in data that we don't have
+  			searchResults.foreach(elem => {
+  				val elemChr = elem._1
+  				val elemBool = elem._2
+  				if (elemBool != true) {
+  					// TODO: putting in correct thing? what are the keys?
+  					put(chr, intl, ks)
+  				}
+  			})
+
+  			// Then fetch that data once we've filled in the gaps
+  			intRDD.multiget(chr, intl, ks)
+  		}
+
   	}
 
   	/* Transparent to the user, should only be called by get if IntervalRDD.get does not return data
@@ -75,6 +98,9 @@ class LazyMaterialization(input: RDD[(Interval[Long], (String, AlignmentRecord))
         val loading = sc.loadParquetAlignments(fp, predicate = Some(pred), projection = Some(proj))
   		val ready: RDD[(Interval[Long], (String, AlignmentRecord))] = loading.map(rec => (new Interval(rec.start, rec.end), ("person1", rec)))
     	intRDD.multiput(chr, intl, ready)
+
+    	// Add an entry into our interval tree
+    	bookkeep.insert(intl, (chr, true))
   		multiget(chr, intl, ks)
   	}
 
@@ -86,16 +112,8 @@ object LazyMaterialization {
   	var conf = new SparkConf(false)
   	var sc = new SparkContext("local", "test", conf)
 
-  	//Create a Lazy Materialization object by feeding in a file to build an RDD from
+  	//Create a Lazy Materialization object by feeding in a file path to build an RDD from
 	def apply(fp: String): LazyMaterialization = {
-		var returned: RDD[(Interval[Long], (String, AlignmentRecord))] = null
-		//TODO: implement pattern matching, for different adapters
-		//TODO: load in multiple files?
-		if (fp.endsWith(".adam")) {
-			println("Trying to load alignments")
-	 		val loading: RDD[AlignmentRecord] = sc.loadParquetAlignments(fp) //TODO: K needs to match types of BDGFormats
-	 		returned = loading.map(rec => (new Interval(rec.start, rec.end), ("person1", rec)))
-		}
-		new LazyMaterialization(returned, fp, sc)
+		new LazyMaterialization(fp, sc)
 	}
 }
